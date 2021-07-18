c26bf4ddda6f3fa7e98914b5d80d1f27eee7ba49
car-r <carson.ri@protonmail.com>
2021-07-18 10:50:51 -0700

    card component update

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { CardContainer, CardEmail, CardImage, CardName, CardNickName } from './CardElements'
+import { CardContainer, CardEmail, CardImage, CardName, } from './CardElements'
 
 const Card = ({robot}) => {
     return (
         <CardContainer>
             <CardImage src={`https://robohash.org/${robot.id}set=set2/?size=180x180`} alt='robot'/>
-            <CardName>{robot.name}</CardName>
-            
+            <CardName>{robot.name}</CardName>  
             <CardEmail>Email: {robot.email}</CardEmail>
         </CardContainer>
     )
